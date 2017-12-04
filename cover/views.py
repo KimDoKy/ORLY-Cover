d88878236a8b0ab52dcf096d4fc86ebe7cf4e39d
Kim Do Ky <makingfunk0@gmail.com>
2017-12-04 22:58:10 +0900

    Form을 통해 데이터를 입력받고, PIL을 통해 이미지 동적 생성하기

--- cover/views.py
+++ cover/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from PIL import Image
+from .forms import CoverForm
 
 def index(request):
-    return render(request, 'cover/index.html')
+    if request.method == 'POST':
+        form = CoverForm(request.POST)
+        if form.is_valid():
+            form.cleaned_data  # 이미지를 생성할 데이터
+            return redirect('cover:index')
+    else:
+        form = CoverForm()
+    return render(request, 'cover/index.html', {
+        'form': form,
+    })
+
+# 위 데이터를 받아서 이미지를 그림
+def image_generator(request):
+    im = Image.new('RGB', (256, 256), 'yellow')
+    response = HttpResponse(content_type='image/jpeg')
+    im.save(response, format='JPEG')
+    return response
