df7507d79ea30484ab331e38bc402c90b7d7f8d8
Kim Do Ky <makingfunk0@gmail.com>
2017-12-06 00:26:50 +0900

    PIL로 이미지에 글자 그림 그리기

--- cover/forms.py
+++ cover/forms.py
@@ -1,6 +1,14 @@
+from urllib.parse import urlencode
 from django import forms
 
-class CoverForm(forms.Form ):
+
+class CoverForm(forms.Form):
     title = forms.CharField()
     top_text = forms.CharField()
     author = forms.CharField()
+
+    @property
+    def get_params(self):
+        if hasattr(self, 'cleaned_data'):
+            return urlencode(self.cleaned_data)
+        return None
--- cover/views.py
+++ cover/views.py
@@ -1,14 +1,14 @@
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 from .forms import CoverForm
 
 def index(request):
     if request.method == 'POST':
         form = CoverForm(request.POST)
         if form.is_valid():
-            form.cleaned_data  # 이미지를 생성할 데이터
-            return redirect('cover:index')
+            pass
     else:
         form = CoverForm()
     return render(request, 'cover/index.html', {
@@ -17,7 +17,24 @@ def index(request):
 
 # 위 데이터를 받아서 이미지를 그림
 def image_generator(request):
-    im = Image.new('RGB', (256, 256), 'yellow')
-    response = HttpResponse(content_type='image/jpeg')
-    im.save(response, format='JPEG')
+    title = request.GET['title']
+    top_text = request.GET['top_text']
+    author = request.GET['author']
+
+    im = Image.new('RGB', (256, 256), 'white')
+
+    ttf_path = settings.ROOT('assets', 'fonts', 'NanumGothic.ttf')
+
+    d = ImageDraw.Draw(im)
+    fnt = ImageFont.truetype(ttf_path, 40)
+    d.text((10, 10), title, font=fnt, fill=(0,255,0,255))
+
+    fnt = ImageFont.truetype(ttf_path, 20)
+    d.text((10, 60), top_text, font=fnt, fill=(0,255,0,255))
+
+    fnt = ImageFont.truetype(ttf_path, 60)
+    d.text((10, 110), author, font=fnt, fill=(0, 255, 0, 255))
+
+    response = HttpResponse(content_type='image/png')
+    im.save(response, format='PNG')
     return response
