5cc80e84f9484f278bb0c1478b4798a53924a054
Kim Do Ky <makingfunk0@gmail.com>
2017-12-08 00:13:32 +0900

    Animal 이미지 합성 및 컬러 적용

--- cover/views.py
+++ cover/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from PIL import Image, ImageFont, ImageDraw
 from .forms import CoverForm
+from .utils import COLOR_CODES
+
 
 def index(request):
     if request.method == 'POST':
@@ -15,21 +17,29 @@ def index(request):
         'form': form,
     })
 
+
 # 위 데이터를 받아서 이미지를 그림
 def image_generator(request):
     title = request.GET['title']
     top_text = request.GET['top_text']
     author = request.GET['author']
     animal_code = request.GET['animal_code']
-    color_code = request.GET['color_code']
+    color_index = request.GET['color_code']
     guide_text = request.GET['guide_text']
     guide_text_placement = request.GET['guide_text_placement']
-        
-    im = Image.new('RGB', (256, 256), 'white')
+
+    animal_path = settings.ROOT('assets', 'animal', '{}.png'.format(animal_code))
+    animal_im = Image.open(animal_path)
+    animal_im = animal_im.resize((200,200))
+
+    color = COLOR_CODES[int(color_index)]
+
+    canvas_im = Image.new('RGB', (500, 700), color)  # 'white')
+    canvas_im.paste(animal_im, (0, 0)) # left, top 지정
 
     ttf_path = settings.ROOT('assets', 'fonts', 'NanumGothic.ttf')
 
-    d = ImageDraw.Draw(im)
+    d = ImageDraw.Draw(canvas_im)
     fnt = ImageFont.truetype(ttf_path, 40)
     d.text((10, 10), title, font=fnt, fill=(0,255,0,255))
 
@@ -38,7 +48,6 @@ def image_generator(request):
 
     fnt = ImageFont.truetype(ttf_path, 60)
     d.text((10, 110), author, font=fnt, fill=(0, 255, 0, 255))
-
     response = HttpResponse(content_type='image/png')
-    im.save(response, format='PNG')
+    canvas_im.save(response, format='PNG')
     return response
